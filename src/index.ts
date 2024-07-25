b797b630875e3402dff89be55f54ed787173197a
Travis Vachon <travis.vachon@gmail.com>
2024-07-24 23:47:46 -0700

    feat: initial commit
    includes a typechecking server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+import fromAsync from 'array-from-async';
+import * as Server from "@ucanto/server";
+import * as Signer from "@ucanto/principal/ed25519";
+import { CAR } from "@ucanto/transport";
+import * as Store from '@web3-storage/capabilities/store'
+
 /**
  * Welcome to Cloudflare Workers! This is your first worker.
  *
@@ -11,8 +17,52 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+const idPromise = Signer.generate()
+
+const createService = (context: any) => ({
+	store: () => {
+		return Server.provide(Store.add, async ({ capability, invocation }) => {
+			return { error: new Server.Failure("unimplemented") }
+		})
+	}
+})
+
+const createServer = async () => {
+	const storedDelegations: Server.API.Delegation[] = []
+	return Server.create({
+		id: await idPromise,
+		codec: CAR.inbound,
+		service: createService({
+			url: new URL('https://example.com'),
+			signer: await idPromise,
+		}),
+		// validate all for now
+		validateAuthorization: async () => ({ ok: {} })
+	})
+}
+
+const serverPromise = createServer()
+
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch (request, env, ctx): Promise<Response> {
+		const server = await serverPromise
+		if (request.body) {
+			const payload = {
+				body: await fromAsync(request.body),
+				headers: Object.fromEntries(request.headers)
+			}
+			const result = server.codec.accept(payload)
+			if (result.error) {
+				throw new Error(`accept failed! ${result.error}`)
+			}
+			const { encoder, decoder } = result.ok
+			const incoming = await decoder.decode(payload)
+			// @ts-ignore not totally sure how to fix the "unknown" casting here or check if it's needed
+			const outgoing = await Server.execute(incoming, server)
+			const response = await encoder.encode(outgoing)
+			return new Response(response.body, { headers: response.headers })
+		} else {
+			throw new Error('no body!')
+		}
 	},
 } satisfies ExportedHandler<Env>;
